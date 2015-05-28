7fd3a1ba8d34fdb026c90fd9e2a12a6d7a24ecdc
柿内暢昌 <nob@studio-kakky.com>
2015-05-29 00:29:41 +0900

    右側に移動する傾向の有るRandomWalkを作成

--- dist/js/classes/classes.js
+++ dist/js/classes/classes.js
@@ -71,4 +71,43 @@ var RandomWalk2 = (function (_RandomWalk) {
 
   return RandomWalk2;
 })(RandomWalk);
+"use strict";
+
+var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(object, property, receiver) { var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc && desc.writable) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+var _inherits = function (subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
+
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+var RandomWalk3 = (function (_RandomWalk) {
+  function RandomWalk3(initX, initY) {
+    _classCallCheck(this, RandomWalk3);
+
+    _get(Object.getPrototypeOf(RandomWalk3.prototype), "constructor", this).call(this, initX, initY);
+  }
+
+  _inherits(RandomWalk3, _RandomWalk);
+
+  _createClass(RandomWalk3, {
+    step: {
+      value: function step() {
+        var choice = Math.random();
+
+        if (choice < 0.4) {
+          this.x++;
+        } else if (choice < 0.6) {
+          this.x--;
+        } else if (choice < 0.8) {
+          this.y++;
+        } else {
+          this.y--;
+        }
+      }
+    }
+  });
+
+  return RandomWalk3;
+})(RandomWalk);
 //# sourceMappingURL=classes.js.map
