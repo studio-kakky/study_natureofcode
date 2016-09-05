e79bf31614d20f94a30e6d2550f6407c5e79abd9
柿内暢昌 <nob@studio-kakky.com>
2016-09-05 23:39:56 +0900

    ExI.4実装

---
+++ src/scripts/articles/chapter0_4_2.js
@@ -0,0 +1,13 @@
+var p;
+
+function setup(){
+  createCanvas(640,360);
+  p = new Painter({r:255,g:0,b:0,a:1});
+  background(255);
+}
+
+function mouseClicked() {
+  console.log(mouseX,mouseY);
+  p.paint(mouseX,mouseY);
+}
+
